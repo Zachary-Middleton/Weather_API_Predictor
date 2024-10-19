eda60fb5980e4a630e6d48d429556b52a5bb87e5
zachary-middleton <zmidd15@wgu.edu>
2024-10-19 12:44:58 -0500

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+import weather
+import unittest
+
+
+def test_weather(self):
+    self.assertEqual(weather.avg_temp5year(# to add later), #correct answer here)
+    self.assertEqual(weather.min_wind5year(# to add later), #correct answer here)
+    self.assertEqual(weather.max_precip_5year(# to add later), #correct answer here)
+
+if '__name__' == 'weather.py__':
+    unittest.weather()
---
+++ weather.py
@@ -0,0 +1,18 @@
+class Weather:
+    def __init__(self):
+        self.Colorado_Springs = ""
+Weather_Colorado_Springs = Weather()
+latitude =
+longitude =
+month =
+day_of_month =
+year =
+avg_temp5year =
+min_temp5year =
+max_temp5year =
+avg_wind5year =
+min_wind5year =
+max_wind5year =
+sum_precip_5year =
+min_precip_5year =
+max_precip_5year =
