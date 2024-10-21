b1d6987a201e12408864dee0c7d7be102ac90013
zachary-middleton <zmidd15@wgu.edu>
2024-10-20 21:31:48 -0500

    test

--- test.py
+++ test.py
@@ -14,12 +14,16 @@ class TestWeather(unittest.TestCase):
 
     def test_initial_longitude(self):
         weather_longitude = Weather(38.8339, -104.8214, 9, 30, 2024)
+
+        # Assert that the longitude is set correctly
         self.assertEqual(weather_longitude.longitude, -104.8214)
 
     def test_five_year_avg_temp(self):
         weather_temp = Weather(38.8339, -104.8214, 9, 30, 2024)
         weather_temp.temp_data()  # Call the method to calculate average temperature
-        self.assertAlmostEqual(weather_temp.five_year_avg_temp, 62.78)  # Compare the attribute, not the object
+
+        # Assert that the five year average temp returns the correct value
+        self.assertAlmostEqual(weather_temp.five_year_avg_temp, 62.78)
 
 
 if __name__ == '__main__':
