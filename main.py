1868d0e967a8806ef017dcb855d5bf02457d7ed8
zachary-middleton <zmidd15@wgu.edu>
2024-10-29 20:10:13 -0500

    test

--- main.py
+++ main.py
@@ -20,61 +20,84 @@ import sqlalchemy
 #
 # if __name__ == "__main__":
 #     main()
-# Base = sqlalchemy.orm.declarative_base()
-#
-# class WeatherData(Base):
-#     __tablename__ = 'weather_data'
-#     id = Column(Integer, primary_key=True)
-#     latitude = Column(Float, nullable=False)
-#     longitude = Column(Float, nullable=False)
-#     month = Column(Integer, nullable=False)
-#     day = Column(Integer, nullable=False)
-#     year = Column(Integer, nullable=False)
-#     five_year_avg_temp = Column(Float, nullable=True)
-#     five_year_min_temp = Column(Float, nullable=True)
-#     five_year_max_temp = Column(Float, nullable=True)
-#     five_year_avg_wind = Column(Float, nullable=True)
-#     five_year_min_wind = Column(Float, nullable=True)
-#     five_year_max_wind = Column(Float, nullable=True)
-#     five_year_sum_precip = Column(Float, nullable=True)
-#     five_year_min_precip = Column(Float, nullable=True)
-#     five_year_max_precip = Column(Float, nullable=True)
-#
-#     def __init__(self, latitude, longitude, month, day, year,
-#                  five_year_avg_temp=None, five_year_min_temp=None, five_year_max_temp=None,
-#                  five_year_avg_wind=None, five_year_min_wind=None, five_year_max_wind=None,
-#                  five_year_sum_precip=None, five_year_min_precip=None, five_year_max_precip=None):
-#         self.latitude = latitude
-#         self.longitude = longitude
-#         self.month = month
-#         self.day = day
-#         self.year = year
-#         self.five_year_avg_temp = five_year_avg_temp
-#         self.five_year_min_temp = five_year_min_temp
-#         self.five_year_max_temp = five_year_max_temp
-#         self.five_year_avg_wind = five_year_avg_wind
-#         self.five_year_min_wind = five_year_min_wind
-#         self.five_year_max_wind = five_year_max_wind
-#         self.five_year_sum_precip = five_year_sum_precip
-#         self.five_year_min_precip = five_year_min_precip
-#         self.five_year_max_precip = five_year_max_precip
-#
-#     def __repr__(self):
-#         return (f"{self.id} {self.latitude} {self.longitude} {self.month} {self.day} {self.year} {self.five_year_avg_temp} {self.five_year_min_temp} {self.five_year_max_temp} {self.five_year_avg_wind} {self.five_year_min_wind} {self.five_year_min_wind} {self.five_year_max_wind} {self.five_year_sum_precip} {self.five_year_min_precip} {self.five_year_max_precip}")
-#
-#
-# def setup_database(db_url='sqlite:///weather_data.db'):
-#     engine = create_engine(db_url)
-#     Base.metadata.create_all(engine)  # Create all tables in the engine
-#     Session = sessionmaker(bind=engine)
-#     return Session()
+Base = sqlalchemy.orm.declarative_base()
+
+class WeatherData(Base):
+    __tablename__ = 'weather_data'
+    id = Column(Integer, primary_key=True)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    month = Column(Integer, nullable=False)
+    day = Column(Integer, nullable=False)
+    year = Column(Integer, nullable=False)
+    five_year_avg_temp = Column(Float, nullable=True)
+    five_year_min_temp = Column(Float, nullable=True)
+    five_year_max_temp = Column(Float, nullable=True)
+    five_year_avg_wind = Column(Float, nullable=True)
+    five_year_min_wind = Column(Float, nullable=True)
+    five_year_max_wind = Column(Float, nullable=True)
+    five_year_sum_precip = Column(Float, nullable=True)
+    five_year_min_precip = Column(Float, nullable=True)
+    five_year_max_precip = Column(Float, nullable=True)
+
+    def __init__(self, latitude, longitude, month, day, year,
+                 five_year_avg_temp=None, five_year_min_temp=None, five_year_max_temp=None,
+                 five_year_avg_wind=None, five_year_min_wind=None, five_year_max_wind=None,
+                 five_year_sum_precip=None, five_year_min_precip=None, five_year_max_precip=None):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.month = month
+        self.day = day
+        self.year = year
+        self.five_year_avg_temp = five_year_avg_temp
+        self.five_year_min_temp = five_year_min_temp
+        self.five_year_max_temp = five_year_max_temp
+        self.five_year_avg_wind = five_year_avg_wind
+        self.five_year_min_wind = five_year_min_wind
+        self.five_year_max_wind = five_year_max_wind
+        self.five_year_sum_precip = five_year_sum_precip
+        self.five_year_min_precip = five_year_min_precip
+        self.five_year_max_precip = five_year_max_precip
+
+    def __repr__(self):
+        return (f"{self.id} {self.latitude} {self.longitude} {self.month} {self.day} {self.year} {self.five_year_avg_temp} {self.five_year_min_temp} {self.five_year_max_temp} {self.five_year_avg_wind} {self.five_year_min_wind} {self.five_year_min_wind} {self.five_year_max_wind} {self.five_year_sum_precip} {self.five_year_min_precip} {self.five_year_max_precip}")
 
 
-# session = setup_database()
-# weather = Weather(38.8339, -104.8214, 9, 30, 2024)
-# insert_weather_data = WeatherData(latitude= weather.latitude, longitude= weather.longitude, month= weather.month, day= weather.day, year = weather.year, five_year_avg_temp= weather.five_year_avg_temp)
-# session.add(insert_weather_data)
-# session.commit()
+def setup_database(db_url='sqlite:///weather_data.db'):
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)  # Create all tables in the engine
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+session = setup_database()
+for year in range(2024, 2019, -1):
+    weather = Weather(38.8339, -104.8214, 9, 30, year)
+    temp = weather.temp_data()
+    insert_weather_data = WeatherData(
+        latitude=weather.latitude,
+        longitude=weather.longitude,
+        month=weather.month,
+        day=weather.day,
+        year=weather.year,
+        five_year_avg_temp=weather.five_year_avg_temp
+    #     five_year_min_temp=,
+    #     five_year_max_temp=,
+    #     five_year_avg_wind=,
+    #     five_year_min_wind=,
+    #     five_year_max_wind=,
+    #     five_year_sum_precip=,
+    #     five_year_min_precip=,
+    #     five_year_max_precip=
+    )
+    session.add(insert_weather_data)
+    # Call methods to get the average temperature, wind speed, and precipitation
+# temp = self.temp_data()
+# max_wind = weather.wind_data()
+# avg_precip = weather.precip_data
+
+
+
+session.commit()
 
 # test_record = WeatherData(latitude= 1, longitude = 2, day = 30, year = 2024, month = 9)
 # record_to_delete = session.query(WeatherData).filter_by(id=1).delete()
--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 import requests
 
 
@@ -35,7 +35,6 @@ class Weather:
             "precipitation_unit": "inch",
             "timezone": "America/Chicago"
         }
-
         # Use the correct function to send the request
         response = requests.get(url, params=params)
 
@@ -45,33 +44,42 @@ class Weather:
         else:
             print("Failed to retrieve data")
             return None
-
     def temp_data(self):
         temperature = []
-        for past_year in range(self.year -5, self.year):
-            data = self.fetch_weather_data(past_year, self.month, self.day)
-            if data:
-                temperature.append(data["temperature_2m_mean"][0])
-        if temperature:
-            self.five_year_avg_temp = sum(temperature) / len(temperature)
-            return self.five_year_avg_temp
+        data = self.fetch_weather_data(self.year, self.month, self)
+        if data:
+            temperature.append(data)
+            print(data)
 
-    def wind_data(self):
-        wind_speed = []
-        for past_year in range(self.year -5, self.year):
-            data = self.fetch_weather_data(past_year, self.month, self.day)
-            if data:
-                wind_speed.append(data["wind_speed_10m_max"][0])
-        if wind_speed:
-            self.five_year_max_wind = max(wind_speed)
-            return self.five_year_max_wind
-    def precip_data(self):
-        precipitation = []
-        for past_year in range(self.year -5, self.year):
-            data = self.fetch_weather_data(past_year, self.month, self.day)
-            if data:
-                precipitation.append(data["precipitation_sum"][0])
-        if precipitation:
-            self.five_year_avg_precip = sum(precipitation)
-            return self.five_year_avg_precip
+weather = Weather(38.8339, -104.8214, 9, 30, 2024)
+weather.temp_data()
+    #
+    # def temp_data(self):
+    #     temperature = []
+    #     for past_year in range(self.year -5, self.year):
+    #         data = self.fetch_weather_data(past_year, self.month, self.day)
+    #         if data:
+    #             temperature.append(data["temperature_2m_mean"][0])
+    #     if temperature:
+    #         self.five_year_avg_temp = sum(temperature) / len(temperature)
+    #         return self.five_year_avg_temp
+    #
+    # def wind_data(self):
+    #     wind_speed = []
+    #     for past_year in range(self.year -5, self.year):
+    #         data = self.fetch_weather_data(past_year, self.month, self.day)
+    #         if data:
+    #             wind_speed.append(data["wind_speed_10m_max"][0])
+    #     if wind_speed:
+    #         self.five_year_max_wind = max(wind_speed)
+    #         return self.five_year_max_wind
+    # def precip_data(self):
+    #     precipitation = []
+    #     for past_year in range(self.year -5, self.year):
+    #         data = self.fetch_weather_data(past_year, self.month, self.day)
+    #         if data:
+    #             precipitation.append(data["precipitation_sum"][0])
+    #     if precipitation:
+    #         self.five_year_avg_precip = sum(precipitation)
+    #         return self.five_year_avg_precip
 
