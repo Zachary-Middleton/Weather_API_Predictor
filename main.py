11fba180b1b75670bdc26a1b4185a97467f281f0
zachary-middleton <zmidd15@wgu.edu>
2024-10-30 18:56:15 -0500

    test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def avg_temp():
     avg_temperature = sum(temperatures) / len(temperatures)
     min_temp = min(temperatures)
     max_temp = max(temperatures)
-    return avg_temperature, min_temp, max_temp
+    return avg_temperature, min_temp, max_temp, temp_2024, temp_2023, temp_2022, temp_2021, temp_2020
 def max_wind():
     wind_2024= weather2024.wind_data()
     wind_2023= weather2023.wind_data()
@@ -25,9 +25,9 @@ def max_wind():
     winds = [wind_2024, wind_2023, wind_2022, wind_2021, wind_2020]
     avg_wind = sum(winds) / len(winds)
     min_wind = min(winds)
-    max_wind = max(winds)
+    maximum_wind = max(winds)
 #
-    return avg_wind, min_wind, max_wind
+    return avg_wind, min_wind, maximum_wind, wind_2024, wind_2023, wind_2022, wind_2021, wind_2020
 def sum_precip():
     precipitation_2024= weather2024.precip_data()
     precipitation_2023= weather2023.precip_data()
@@ -40,7 +40,7 @@ def sum_precip():
     min_precip = min(precipitations)
     max_precip = max(precipitations)
 
-    return avg_precip, min_precip, max_precip, sum_precips
+    return avg_precip, min_precip, max_precip, sum_precips, precipitation_2024, precipitation_2023, precipitation_2022, precipitation_2021, precipitation_2020
 
 
 weather2024 = Weather(38.8339, -104.8214, 9, 30, 2024)
@@ -50,21 +50,10 @@ weather2021 = Weather(38.8339, -104.8214, 9, 30, 2021)
 weather2020 = Weather(38.8339, -104.8214, 9, 30, 2020)
 
 
-average_temperature, min_temp, max_temp = avg_temp()
-avg_wind, min_wind, max_wind = max_wind()
-avg_precip, min_precip, max_precip, sum_precips = sum_precip()
+average_temperature, min_temp, max_temp, temp_2024, temp_2023, temp_2022, temp_2021,temp_2020 = avg_temp()
+average_wind, min_wind, maximum_wind, wind_2024, wind_2023, wind_2022, wind_2021, wind_2020 = max_wind()
+average_precip, min_precip, max_precip, sum_precips, precipitation_2024, precipitation_2023,precipitation_2022, precipitation_2021, precipitation_2020 = sum_precip()
 
-#
-# print(f"Average Temperature: {average_temperature}")
-# print(f"Minimum Temperature: {min_temp}")
-# print(f"Maximum Temperature: {max_temp}")
-# print(f"Average Wind: {avg_wind}")
-# print(f"Minimum Wind: {min_wind}")
-# print(f"Maximum Wind: {max_wind}")
-# print(f"Average Precip: {avg_precip}")
-# print(f"Minimum Precip: {min_precip}")
-# print(f"Maximum Precip: {max_precip}")
-# print(f"Sum Precip: {sum_precips}")
 
 Base = sqlalchemy.orm.declarative_base()
 
@@ -115,24 +104,93 @@ def setup_database(db_url='sqlite:///weather_data.db'):
     Session = sessionmaker(bind=engine)
     return Session()
 
-# session = setup_database()
+session = setup_database()
 
 
 entry_2024 = WeatherData(
-    latitude=weather.latitude,
-    longitude=weather.longitude,
+    latitude=weather2024.latitude,
+    longitude=weather2024.longitude,
     month=9,
     day = 30,
     year = 2024,
-    five_year_avg_temp = average_temp,
-    five_year_min_temp =  min_temp,
-    five_year_max_temp = max_temp,
-    five_year_avg_wind = avg_wind,
-    five_year_min_wind = min_wind,
-    five_year_max_wind = max_wind,
-    five_year_sum_precip = sum_precip,
-    five_year_min_precip = min_precip,
-    five_year_max_precip = max_precip,
+    five_year_avg_temp = temp_2024,
+    five_year_min_temp =  temp_2024,
+    five_year_max_temp = temp_2024,
+    five_year_avg_wind = wind_2024,
+    five_year_min_wind = wind_2024,
+    five_year_max_wind = wind_2024,
+    five_year_sum_precip = precipitation_2024,
+    five_year_min_precip = precipitation_2024,
+    five_year_max_precip = precipitation_2024
 )
+# entry_2023 = WeatherData(
+#     latitude=weather2023.latitude,
+#     longitude=weather2023.longitude,
+#     month=9,
+#     day = 30,
+#     year = 2023,
+#     five_year_avg_temp = average_temperature,
+#     five_year_min_temp =  min_temp,
+#     five_year_max_temp = max_temp,
+#     five_year_avg_wind = average_wind,
+#     five_year_min_wind = min_wind,
+#     five_year_max_wind = maximum_wind,
+#     five_year_sum_precip = sum_precips,
+#     five_year_min_precip = min_precip,
+#     five_year_max_precip = max_precip
+# )
+# entry_2022 = WeatherData(
+#     latitude=weather2022.latitude,
+#     longitude=weather2022.longitude,
+#     month=9,
+#     day = 30,
+#     year = 2022,
+#     five_year_avg_temp = average_temperature,
+#     five_year_min_temp =  min_temp,
+#     five_year_max_temp = max_temp,
+#     five_year_avg_wind = average_wind,
+#     five_year_min_wind = min_wind,
+#     five_year_max_wind = maximum_wind,
+#     five_year_sum_precip = sum_precips,
+#     five_year_min_precip = min_precip,
+#     five_year_max_precip = max_precip
+# )
+# entry_2021 = WeatherData(
+#     latitude=weather2021.latitude,
+#     longitude=weather2021.longitude,
+#     month=9,
+#     day = 30,
+#     year = 2021,
+#     five_year_avg_temp = average_temperature,
+#     five_year_min_temp =  min_temp,
+#     five_year_max_temp = max_temp,
+#     five_year_avg_wind = average_wind,
+#     five_year_min_wind = min_wind,
+#     five_year_max_wind = maximum_wind,
+#     five_year_sum_precip = sum_precips,
+#     five_year_min_precip = min_precip,
+#     five_year_max_precip = max_precip
+# )
+# entry_2020 = WeatherData(
+#     latitude=weather2020.latitude,
+#     longitude=weather2020.longitude,
+#     month=9,
+#     day = 30,
+#     year = 2020,
+#     five_year_avg_temp = average_temperature,
+#     five_year_min_temp =  min_temp,
+#     five_year_max_temp = max_temp,
+#     five_year_avg_wind = average_wind,
+#     five_year_min_wind = min_wind,
+#     five_year_max_wind = maximum_wind,
+#     five_year_sum_precip = sum_precips,
+#     five_year_min_precip = min_precip,
+#     five_year_max_precip = max_precip
+# )
 session.add(entry_2024)
+# session.add(entry_2023)
+# session.add(entry_2022)
+# session.add(entry_2021)
+# session.add(entry_2020)
+
 session.commit()
